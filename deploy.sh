a4d54782917bb676f0631525629f5c0b88ed0d07
尤炎平 <youyp@fjcredit.com>
2023-06-16 17:41:36 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -4,14 +4,13 @@
 set -e
 
 git init
-git add -A
+git add .
 git commit -m 'deploy'
 
 # 如果部署到 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果是部署到 https://<USERNAME>.github.io/<REPO>
-git pull
 git push -u origin main
 
 cd -
