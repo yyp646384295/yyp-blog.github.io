0c928f64978acf208df5160ba3e9b56bcb9297d3
尤炎平 <youyp@fjcredit.com>
2023-06-16 16:43:09 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,25 @@
 #!/usr/bin/env sh
 
-# 确保脚本抛出遇到的错误
+# 忽略错误
 set -e
 
-# 生成静态文件
+# 构建
 npm run docs:build
 
-# 进入生成的文件夹
-cd docs/.vuepress/dist
+# 进入待发布的目录
+cd docs/.vitepress/dist
+
+# 如果是发布到自定义域名
+# echo 'www.example.com' > CNAME
 
 git init
 git add -A
 git commit -m 'deploy'
 
-# 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/yyp646384295/yyp-blog.github.io main:gh-pages
+# 如果部署到 https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# 如果是部署到 https://<USERNAME>.github.io/<REPO>
+git push -f https://github.com/yyp646384295/yyp-blog.github.io master:gh-pages
 
 cd -
