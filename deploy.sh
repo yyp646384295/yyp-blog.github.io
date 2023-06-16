db2556b5d73c83502c38e9073f73af215cebb6ab
尤炎平 <youyp@fjcredit.com>
2023-06-16 17:04:25 +0800

    log

--- deploy.sh
+++ deploy.sh
@@ -3,15 +3,6 @@
 # 忽略错误
 set -e
 
-# 构建
-npm run docs:build
-
-# 进入待发布的目录
-cd docs/.vitepress/dist
-
-# 如果是发布到自定义域名
-# echo 'www.example.com' > CNAME
-
 git init
 git add -A
 git commit -m 'deploy'
@@ -20,6 +11,7 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果是部署到 https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/yyp646384295/yyp-blog.github.io main:gh-pages
+git pull
+git push -u origin main
 
 cd -
